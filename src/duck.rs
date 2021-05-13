d8b0aacafec351325b01edbd201df051f3a0f7a8
corlesss <jazzchamp600@gmail.com>
2021-05-13 16:26:29 -0600

    Add duck object, random duck noises

---
+++ src/duck.rs
@@ -0,0 +1,31 @@
+
+pub mod duck {
+    use rand::seq::SliceRandom;
+    use crate::messages::message::Message;
+
+    pub fn new() -> Message {
+        let text = random_quack();
+        let color = crossterm::style::Color::Yellow;
+        let name = "Duck";
+
+        let duck_message = Message::new(text, name, 'l', color);
+
+        duck_message
+    }
+
+    fn random_quack() -> &'static str {
+        let quack_list = vec![
+            "quak",
+            "quack",
+            "quackquackqucak",
+            "🦆",
+            "*squeak*",
+            "*flap*",
+            "float",
+        ];
+
+        let quack = quack_list.choose(&mut rand::thread_rng()).unwrap();
+
+        quack
+    }
+}
